999b0d7603c607de42ce60f22da40466464dfdf9
KrissLa <arsavit@gmail.com>
2020-11-14 19:13:06 +0300

    edit captcha solving

--- roll.py
+++ roll.py
@@ -10,14 +10,12 @@ from selenium.webdriver.common.keys import Keys
 from db_api.db_commands import get_profiles, update_profile, add_roll
 from db_api.db_tortoise import on_startup
 
-
 utc = pytz.UTC
 
 
 async def start_rolling():
     """Запускаем"""
     await on_startup()
-
     while True:
         profiles = await get_profiles()
         for pd in profiles:
@@ -30,11 +28,13 @@ async def start_rolling():
             sleep(3)
             browser.get('https://freebitco.in/')
             sleep(5)
+            _cancel_notification(browser)
             button = _get_button_or_none(browser)
             if button:
                 logger.info('Кнопка доступна!')
                 if _captcha_is_needed(browser):
                     logger.info('Капча нужна!')
+                    _click_i_not_robot(browser)
                     _wait_captcha_complete(browser)
                 else:
                     logger.info('Капчи нет!')
@@ -60,14 +60,14 @@ async def start_rolling():
 def roll_one():
     options = webdriver.ChromeOptions()
     options.add_argument("user-data-dir=C:\\Users\\Administrator\\AppData\\Local\\Google\\Chrome\\User Data")
-    options.add_argument(f"--profile-directory=Profile 23")
+    options.add_argument(f"--profile-directory=Profile 24")
     # options.add_argument('headless')
     browser = webdriver.Chrome(chrome_options=options)
     sleep(2)
     browser.get('https://freebitco.in/')
     sleep(2)
     account_balance = {
-        'account': 'Profile 23',
+        'account': 'Profile 24',
         'balance': _get_balance_or_none(browser),
         'date': datetime.now(timezone('Europe/Moscow')).strftime("%d.%m.%Y"),
         'time': datetime.now(timezone('Europe/Moscow')).strftime("%H:%M")
@@ -78,13 +78,29 @@ def roll_one():
         logger.info('Кнопка доступна!')
         if _captcha_is_needed(browser):
             logger.info('Капча нужна!')
-            _wait_captcha_complete(browser)
-            button.send_keys(Keys.RETURN)
+            # _wait_captcha_complete(browser)
+            _click_i_not_robot(browser)
+            sleep(999)
         else:
             logger.info('Капчи нет!')
-            button.send_keys(Keys.RETURN)
+    button.send_keys(Keys.RETURN)
     sleep(2)
     browser.close()
+
+
+def _cancel_notification(browser):
+    """Отменяем уведомления"""
+    try:
+        modal = browser.find_element_by_id('push_notification_modal')
+        print('Нашел окно')
+    except:
+        modal = None
+    try:
+        if modal:
+            button = browser.find_element_by_css_selector('.pushpad_deny_button')
+            button.click()
+    except:
+        pass
 
 
 def _get_button_or_none(browser):
@@ -126,12 +142,28 @@ def _captcha_is_needed(browser):
         return False
 
 
+def _click_i_not_robot(browser):
+    """Нажатие на я не робот"""
+    try:
+        i_not_robot_button = browser.find_element_by_css_selector('.g-recaptcha div div')
+        ActionChains(browser).move_to_element(i_not_robot_button).perform()
+        i_not_robot_button.click()
+    except Exception as err:
+        logger.error(err)
+    sleep(3)
+
+
+
 def _wait_captcha_complete(browser):
     """Ожидание решения капчи"""
     while True:
         status = browser.find_element_by_css_selector('.status')
-        if status.text == "Solved":
+        if status.text in ["Solved", "AntiCaptcha"]:
             return True
+        elif status.text in ["Could not load captcha provider widget in worker browser. Please try again.",
+                             "Outdated, should be solved again"]:
+            browser.get('https://freebitco.in/')
+            sleep(5)
         sleep(3)
 
 
@@ -162,3 +194,5 @@ def _check_for_time(last_time):
             logger.info('Выхожу из функции _check_for_time')
             return
         sleep(20)
+
+
